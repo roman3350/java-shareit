820175a5fde4ae3b044c07cc3eb40b2ef5a0bdd7
Roman <roman33350@yandex.ru>
2023-11-06 04:22:06 +0300

    feat: Разделить приложение на микросервисы

--- gateway/src/main/java/ru/practicum/shareit/utilites/exception/ValidationTime.java
+++ gateway/src/main/java/ru/practicum/shareit/utilites/exception/ValidationTime.java
@@ -1,7 +1,7 @@
 package ru.practicum.shareit.utilites.exception;
 
-public class ValidationTime extends RuntimeException{
-    public ValidationTime(String s){
+public class ValidationTime extends RuntimeException {
+    public ValidationTime(String s) {
         super(s);
     }
 }
